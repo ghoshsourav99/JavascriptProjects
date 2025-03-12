eb1c47f646c0ea310349114ce0e434b2dd5ca581
Sourav Ghosh <146316533+ghoshsourav99@users.noreply.github.com>
2025-03-13 00:37:44 +0530

    Update chaiaurcode.js

--- 8-typer/chaiaurcode.js
+++ 8-typer/chaiaurcode.js
@@ -1,8 +1,37 @@
-const typedTextSpan = document.querySelector('.typed-text');
-const cursor = document.querySelector('.cursor');
+const typedTextSpan = document.querySelector(".typed-text");
+const cursorSpan = document.querySelector(".cursor");
 
-const words = ['Love', 'Jhakaas', 'mast', 'dhinchak', 'Weird'];
+const words = ["Awesome", "Powerful", "Dynamic", "Versatile", "Fast"];
+let wordIndex = 0; 
+let charIndex = 0; 
+let isDeleting = false;
 
-function type() {}
+function typeEffect() {
+  const currentWord = words[wordIndex]; 
+  const currentChars = currentWord.substring(0, charIndex); 
 
-function erase() {}
+  typedTextSpan.textContent = currentChars; 
+
+  if (!isDeleting) {
+    charIndex++;
+    if (charIndex > currentWord.length) {
+      isDeleting = true; 
+      setTimeout(typeEffect, 1000); 
+      return;
+    }
+  } else {
+    charIndex--;
+    if (charIndex === 0) {
+      isDeleting = false; 
+      wordIndex = (wordIndex + 1) % words.length; 
+    }
+  }
+
+  setTimeout(typeEffect, isDeleting ? 100 : 200)
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  setTimeout(typeEffect, 1000);
+});
+
+
